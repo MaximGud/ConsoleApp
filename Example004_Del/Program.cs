172cd7d8f7190808bcae8fe2d519e10431ae46a0
maxim_gud <mgudoshnikov@mail.ru>
2023-02-09 20:32:15 +0300

    added random

--- Example004_Del/Program.cs
+++ Example004_Del/Program.cs
@@ -1,4 +1,6 @@
-﻿double numberA=12;
-double numberB=5;
+﻿int numberA=new Random().Next(1,10); // 1,2,3,...9
+Console.WriteLine(numberA);
+int numberB=new Random().Next(1,10);
+Console.WriteLine(numberB);
 double result = numberA / numberB;
 Console.WriteLine(result);
