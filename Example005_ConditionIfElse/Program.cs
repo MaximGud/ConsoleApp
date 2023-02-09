a06318d52b983741850bbe4a88f5b7c2a0a6c72d
maxim_gud <mgudoshnikov@mail.ru>
2023-02-09 20:44:43 +0300

    added 5 proj Name

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя:");
+String username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+  Console.WriteLine("Ух ты, это же Маша, привет!");
+}
+else
+{
+  Console.Write("Привет, ");
+  Console.Write(username);
+}
